5fa59b3ac533b6556a12f170cfbcd42d26fb84d8
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2021-10-24 18:45:49 +0100

    Update report.py

--- report/report.py
+++ report/report.py
@@ -8,7 +8,13 @@ class report(commands.Cog):
     @commands.command()
     async def report(self, ctx, user: discord.Member, reason:str, *, proof:str):
         reports_channel = self.bot.get_channel(822612395516100618)
-        embed = discord.Embed(title="New report", description=f'{ctx.author.mention} is reporting {user.mention} for {reason} \n Proof: {proof}')
+        embed = discord.Embed(title="New report", description=f'{ctx.author.mention} is reporting {user.mention} for {reason} \n\n Proof: {proof}')
+        await reports_channel.send(embed=embed)
+     
+    @commands.command()
+    async def appeal(self, ctx, punishment:str, reasonfor:str, *, appeal:str):
+        appeals_channel = self.bot.get_channel(822612512942850059)
+        embed = discord.Embed(title="New appeal", description=f'{ctx.author.mention} is appealing a {punishment} for {reason} \n\n Appeal: {appeal}')
         await reports_channel.send(embed=embed)
 
 def setup(bot):
