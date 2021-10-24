3369c35de3041069b3bda8bf09244e345cc45f0a
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2021-10-24 18:19:37 +0100

    Update report.py

--- report/report.py
+++ report/report.py
@@ -1,1 +1,14 @@
-test
+from discord.ext import commands
+
+class report(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def report(self, ctx, user: discord.member, reason:str, *, proof:str):
+        reports_channel = bot.get_channel(822612395516100618)
+        embed = discord.Embed(title="New report", description=f'{ctx.author.mention} is reporting {user.mention} for {reason} \n Proof: {proof}')
+        await reports_channel.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(report(bot))
