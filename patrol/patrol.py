7bdc8b8b0e1fd2a2f6fb05663a57fa51d783b425
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2021-10-25 14:57:14 +0100

    Update patrol.py

--- patrol/patrol.py
+++ patrol/patrol.py
@@ -1,2 +1,14 @@
-def __init__(self, bot):  # in the class init
-    self.coll = bot.api.get_plugin_partition(self)
+import discord
+from discord.ext import commands
+
+class report(commands.Cog):
+    def __init__(self, bot):
+        self.coll = bot.api.get_plugin_partition(self)
+        self.bot = bot
+
+    @commands.command()
+    async def report(self, ctx):
+        
+       
+def setup(bot):
+    bot.add_cog(report(bot))
