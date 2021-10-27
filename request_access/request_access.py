4399baeb9af39e8d4fea80f2a28f267ba4941032
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2021-10-27 11:05:44 +0100

    Update request_access.py

--- request_access/request_access.py
+++ request_access/request_access.py
@@ -1,1 +1,13 @@
 import discord
+from discord.ext import commands
+
+class report(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def request(self, ctx, role):
+        if role == "snr" or "s&r":
+       
+def setup(bot):
+    bot.add_cog(report(bot))
