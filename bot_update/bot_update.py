4ff9cff795e08af4c656734a9f8a23d87dcc11a4
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2021-10-27 11:49:42 +0100

    Update bot_update.py

--- bot_update/bot_update.py
+++ bot_update/bot_update.py
@@ -1,1 +1,14 @@
 import discord
+from discord.ext import commands
+
+class report(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def update(self, ctx, *, message):
+        channel = 
+       
+def setup(bot):
+    bot.add_cog(report(bot))
