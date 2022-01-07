e2b529645033e3217982db9c3b00aa2725474b9d
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2022-01-07 17:36:05 +0000

    Update embed.py

--- embed/embed.py
+++ embed/embed.py
@@ -1,1 +1,12 @@
+import discord
+import asyncio
+from discord.ext import commands, tasks
 
+class utility(commands.Cog):  
+    def __init__(self, bot):
+        self.bot = bot
+        
+        
+
+def setup(bot):
+    bot.add_cog(utility(bot))
