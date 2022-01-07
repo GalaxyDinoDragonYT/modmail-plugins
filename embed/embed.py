4ce447da31153b2f3e923dce0844e813133dfd89
GalaxyDinoDragonYT <78411327+GalaxyDinoDragonYT@users.noreply.github.com>
2022-01-07 17:40:56 +0000

    Update embed.py

--- embed/embed.py
+++ embed/embed.py
@@ -1,12 +1,15 @@
 import discord
-import asyncio
-from discord.ext import commands, tasks
+from discord import *
+from discord.ext import commands
 
-class utility(commands.Cog):  
+class embed(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-        
 
+    @commands.command()
+    async def newEmbed(self, ctx, title1, *, description1):
+       await ctx.send(embed=discord.Embed(title=title, description=description1).set_author(name=ctx.author.display_name)
+
+    
 def setup(bot):
-    bot.add_cog(utility(bot))
+    bot.add_cog(embed(bot))
